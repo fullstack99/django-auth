ec0492e270985c2df0331ad05b2c7491e1cdf330
fullstack99 <success1989127@gmail.com>
2020-01-28 12:23:23 -0300

    add messages functions

--- apps/register/views.py
+++ apps/register/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib import messages
+from django.http import JsonResponse
 import bcrypt
+import json
 from .models import User
 
 def index(request):
@@ -12,13 +14,23 @@ def register(request):
         if len(errors):
             for tag, error in errors.items():
                 messages.error(request, error, extra_tags=tag)
-                return redirect('/register')
+            error_messages = messages.get_messages(request)
+            for error_message in error_messages:
+                message = error_message.message
+
+                return JsonResponse({
+                    "tag": error_message.tags,
+                    "message": message
+                })
 
         hashed_password = bcrypt.hashpw(request.POST['password'].encode('utf-8'), bcrypt.gensalt())
         user = User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], password=hashed_password, email=request.POST['email'])
         user.save()
         request.session['id'] = user.id
-        return redirect('/success')
+
+        return JsonResponse({
+            "tag": "success"
+        })
     else:
         return render(request, 'register/signup.html')
 
